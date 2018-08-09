41389b5788d0e0f4ef04c820edf5bd98e38f15ee
Yoshua Elmaryono <yoshuaelmaryono@yoshua-e.local>
2018-08-09 17:40:50 +0700

    add next button and use uitextview

--- bully-story/ViewController.swift
+++ bully-story/ViewController.swift
@@ -9,17 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Lifecycle Hooks
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Actions
+    @IBAction func next(_ sender: UIButton) {
+        print(11)
     }
-
-
+    
 }
 
