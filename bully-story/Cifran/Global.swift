e2d6c17daf7945a1806e4d72a1c6ae4e7d19b3ba
dotm <desde.el.mar96@gmail.com>
2018-08-22 16:49:08 +0700

    partially add encoding and decoding for events

--- bully-story/Cifran/Global.swift
+++ bully-story/Cifran/Global.swift
@@ -14,8 +14,38 @@ var empathymeter: Int = 0
 func saveGlobalData(){
     UserDefaults.standard.set(username, forKey: "username")
     UserDefaults.standard.set(empathymeter, forKey: "empathymeter")
+    
+    let events = Events(events: [
+        [
+            .setBackgroundImage(imageName: "classroom"),
+            .presentDialog(
+                characterName: "Jane",
+                characterNamePosition: .left,
+                characterImage: "Jane_Smile",
+                characterImagePosition: .left,
+                dialogText: "Hi there. I’m Jane. What’s your name?"
+            ),
+            ],[
+                .setBackgroundImage(imageName: "classroom"),
+                .presentDialog(
+                    characterName: "Jane",
+                    characterNamePosition: .left,
+                    characterImage: "Jane_Smile",
+                    characterImagePosition: .left,
+                    dialogText: "Hi there. I’m Jane. What’s your name?"
+                ),
+                ],
+              ])
+//    let encoded = NSKeyedArchiver.archivedData(withRootObject: events)
+//
+//    UserDefaults.standard.set(encoded, forKey: "test")
+
 }
 func loadGlobalData(){
     username = UserDefaults.standard.string(forKey: "username")!
     empathymeter = UserDefaults.standard.integer(forKey: "empathymeter")
+    
+//    let decoded = UserDefaults.standard.object(forKey: "test") as! Data
+//    let obj = NSKeyedUnarchiver.unarchiveObject(with: decoded) as! Events
+//    print(obj.events)
 }
