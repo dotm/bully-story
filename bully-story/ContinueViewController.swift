e7bc52f9a5a7c00ced20d099b5e83387ca2e580f
Rayo Roderik <rayoroderik@gmail.com>
2018-08-27 00:51:45 +0700

    fix main menu add continue

---
+++ bully-story/ContinueViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ContinueViewController.swift
+//  bully-story
+//
+//  Created by Rayo Roderik on 27/08/18.
+//  Copyright © 2018 Yoshua Elmaryono. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class ContinueViewController: UIViewController {
+    var bgAudio : AVAudioPlayer!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func gotoActOne(_ sender: Any) {
+        
+    }
+    
+    @IBAction func gotoActTwo(_ sender: Any) {
+    }
+    
+    @IBAction func gotoActThree(_ sender: Any) {
+    }
+    
+    @IBAction func gotoActFour(_ sender: Any) {
+    }
+    
+   
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
